eec929ea8f80f34a7dfe6d84e033357508ca157f
Walter <walterfilho1000@gmail.com>
2023-05-09 21:28:00 -0300

    atividade criação de rota para novos modulos/componentes

---
+++ src/app/shared/model/frutasList.ts
@@ -0,0 +1,15 @@
+export const frutasList = [
+  {
+    nome: 'Maçã',
+    unidades: '500',
+    valorKG: '15' },
+  {
+    nome: 'Pêra',
+    unidades: '200',
+    valorKG: '20' },
+  {
+    nome: 'Uva',
+    unidades: '100',
+    valorKG: '25' }
+];
+
---
+++ src/app/shared/model/usuarioslist.ts
@@ -0,0 +1,15 @@
+export const usuarioslist = [
+  {
+    nome: 'walter',
+    cpf: '12345578921',
+    idade: '22' },
+  {
+    nome: 'Will',
+    cpf: '86234123411',
+    idade: '28' },
+  {
+    nome: 'Bor',
+    cpf: '23533412121',
+    idade: '62' },
+];
+
